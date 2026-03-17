3591c6c79d9a6c43718332c80bee42e4f334099f
hmaskell <23707057+hmaskell@users.noreply.github.com>
2026-03-16 20:11:08 -0700

    move files out of dev folder

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import tailwindcss from '@tailwindcss/vite';
 // https://astro.build/config
 export default defineConfig({
 
-  site: 'https://hmaskell.github.io',
+  site: 'https://www.roguewave.design',
   vite: {
     plugins: [tailwindcss()]
   }
